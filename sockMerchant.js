6cba5fff4d9bc32a59ecc7a52a49444eb1f055ad
cammac <camm@rocktechcomputer.com>
2019-11-22 12:38:01 -0800

    updated sockMerchant

--- sockMerchant.js
+++ sockMerchant.js
@@ -3,18 +3,34 @@
 arr = ["pink", "green", "green", "red", "green", "pink"]
 
 function sockMerchant(arr) {
-    let len = arr.length
-    let pairs = 0
-    let colors = {}
-    for (let i = 0; i < len; i++) {
-        if (colors[arr[i]]) {
-            pairs++
-            colors[arr[i]] = 0
+    // create a set
+    let socks = new Set()
+    let count = 0
+
+    for (let sock of arr) {
+        if (socks.has(sock)) {
+            count++
+            socks.delete(sock)
         } else {
-            colors[arr[i]] = 1
+            socks.add(sock)
         }
     }
-    return pairs
+    return count
+
+
+    // let len = arr.length
+    // let pairs = 0
+    // let colors = {}
+    // for (let i = 0; i < len; i++) {
+    //     if (colors[arr[i]]) {
+    //         pairs++
+    //         colors[arr[i]] = 0
+    //     } else {
+    //         colors[arr[i]] = 1
+    //     }
+    // }
+    // return pairs
+
 
     // sorting the array first -- much slower
 
