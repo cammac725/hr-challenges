84d9929c4f0fd2e787c44dfa1c856e1ae55d668a
cammac <camm@rocktechcomputer.com>
2019-12-11 09:55:28 -0800

    added ll_merge_point solution

--- ll_merge_point.py
+++ ll_merge_point.py
@@ -6,21 +6,35 @@
 #
 #
 
+
+# def findMergeNode(head1, head2):
+#     s = set()
+
+#     currA = head1
+#     currB = head2
+
+#     while currA:
+#         s.add(currA)
+#         currA = currA.next
+
+#     while currB:
+#         if currB in s:
+#             return currB.data
+#         currB = currB.next
+
+
+##### OR
+
+
 def findMergeNode(head1, head2):
-
-    # traverse the first list and use a set to store
-    # those values
-    # check the second list for a value already in the store
-    s = set()
-
     currA = head1
     currB = head2
 
     while currA:
-        s.add(currA)
+        currA.visited = True
         currA = currA.next
 
     while currB:
-        if currB in s:
+        if hasattr(currB, 'visited'):
             return currB.data
         currB = currB.next
